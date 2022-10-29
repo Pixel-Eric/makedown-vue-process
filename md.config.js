742932ba3ab33ae9941957d4d5cf1a96783c07ee
Eric <487737744@qq.com>
2022-10-29 17:10:17 +0800

    fix: 移动测试用例位置

--- md.config.js
+++ md.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   // md-process read file path
-  path: path.resolve(__dirname, './example/md'),
+  path: path.resolve(__dirname, './example/'),
   output: {
     path: path.resolve(__dirname, 'docs')
   },

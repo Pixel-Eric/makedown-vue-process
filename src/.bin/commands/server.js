49d0dae61bcdb2db1700f2f7940e7bbe3d33e71d
Eric <487737744@qq.com>
2022-12-07 00:13:21 +0800

    fix: 剔除copyDir非通用代码

--- src/.bin/commands/server.js
+++ src/.bin/commands/server.js
@@ -2,8 +2,15 @@ const path = require('path');
 const copyDir = require('../utils/copyDir');
 const ts = require('typescript');
 
+const dirs = ["compiler", "template", "public"];
+
 module.exports = function () {
-  copyDir();
+  dirs.forEach(dir => {
+    let source = path.join(process.cwd(), "/src/", dir);
+    let destination = path.join(process.cwd(), "/dist", dir);
+    copyDir(source, destination);
+  })
+
   const { server } = require(path.resolve(process.cwd(), './dist/package/coverts/init'));
   server();
 }
--- src/.bin/utils/copyDir.js
+++ src/.bin/utils/copyDir.js
@@ -1,12 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const dirs = ["compiler", "template", "public"];
 
-module.exports = function () {
-  dirs.forEach(dir => {
-    let source = path.join(process.cwd(), "/src/", dir);
-    let destination = path.join(process.cwd(), "/dist", dir);
-
-    fs.cpSync(source, destination, { recursive: true, force: true });
-  })
+module.exports = function (source, destination) {
+  fs.cpSync(source, destination, { recursive: true, force: true });
 }
